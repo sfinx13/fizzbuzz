68e16d00c2033a3907f703a9916ef634d7223ef7
Saidi AHAMADA <AHAMADA@osmos.local>
2018-12-22 00:43:21 +0100

    format

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,22 @@
 /*
-Critère d'acceptance FizzBuzz: 
-Ecrire une fonction qui retourne pour chaque nombre entre 1 à 100 :
-- "Fizz" au lieu du nombre si multiple de 3
-- "Buzz" au lieu du nombre si multiple de 5
-- "FizzBuzz" au lieu du nombre si multiple de 3 et 5
-- "Renvoie le nombre
--  Renvoie une exception si nombre n'est pas etre 1 et 100 inclus
-*/ 
+Acceptance FizzBuzz: 
+Write a function that returns for a number between 1 to 100:
 
-function fizzBuzz(number) {
+Returns an exception if number is not 1 and 100 inclusive
+"Fizz" instead of the number if multiple of 3
+"Buzz" instead of the number if multiple of 5
+"FizzBuzz" instead of the number so multiple of 3 and 5
+Returns the number if no multiple of 3 or 5
+*/
+function fizzBuzz(number) 
+{
+    if (number>100 || number <0) throw 'Error';
 
-    if (number>100 || number <0) throw 'error';
-
-    return  (0 === number%3 && 0 === number%5)  ? 
+    return  0 === number%3 && 0 === number%5 ? 
                 'FizzBuzz': 
-                    ( 0 === number%3  ? 
+                    ( 0 === number%3 ? 
                         'Fizz' : ( 0 === number%5 ? 
-                                    'Buzz' : number
-                                 )
+                                    'Buzz' : number)
                     )
 }
 

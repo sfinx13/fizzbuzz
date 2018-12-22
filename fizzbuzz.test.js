f917f3efbc4e508fe6a25e76ccd2170836d080ac
Saidi AHAMADA <AHAMADA@osmos.local>
2018-12-22 09:48:03 +0100

    refacto

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,30 +1,29 @@
 const fizzBuzz = require('./fizzbuzz.js');
 
-test('4 return 4',()=>{
+test('4 returns 4',() => {
 	expect(fizzBuzz(4)).toBe(4);
 });
 
-test('33 return Fizz',()=>{
+test('33 returns Fizz',() => {
 	expect(fizzBuzz(33)).toBe('Fizz');
 });
 
-test('55 retur Buzz',()=>{
+test('55 returns Buzz',() => {
 	expect(fizzBuzz(55)).toBe('Buzz');
 });
 
-test('90 retur FizzBuzz', ()=>{
+test('90 returns FizzBuzz', () => {
 	expect(fizzBuzz(90)).toBe('FizzBuzz');
 });
 
-test('101 retur error', () => {
-  expect(() =>{
+test('101 returns Error', () => {
+  expect(() => {
   	fizzBuzz(101);
   	}).toThrow('Error');
 });
 
-test('-10 retur error', () => {
-  expect(() =>{
+test('-10 returns Error', () => {
+  expect(() => {
   	fizzBuzz(-10);
   	}).toThrow('Error');
 });
-
